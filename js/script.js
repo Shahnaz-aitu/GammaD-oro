60167d9bb82c04937a19374edbce4ef2c76876dd
Shahnaz-aitu <240843@astanait.edu.kz>
2026-04-22 15:55:10 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const item = header.parentElement;
+        item.classList.toggle('active');
+    });
+});
